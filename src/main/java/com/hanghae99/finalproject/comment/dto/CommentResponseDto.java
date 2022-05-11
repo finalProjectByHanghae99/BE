f799307c719e49da25784040fb09320d16c22359
hyemco <hyemco@gmail.com>
2022-05-11 23:48:31 +0900

    comment 작성시간 response format 수정

--- src/main/java/com/hanghae99/finalproject/comment/dto/CommentResponseDto.java
+++ src/main/java/com/hanghae99/finalproject/comment/dto/CommentResponseDto.java
@@ -1,13 +1,9 @@
 package com.hanghae99.finalproject.comment.dto;
 
 import com.hanghae99.finalproject.comment.model.Comment;
-import com.hanghae99.finalproject.user.dto.MyPageDto;
-import com.hanghae99.finalproject.user.model.User;
+import com.hanghae99.finalproject.timeConversion.TimeConversion;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @NoArgsConstructor
@@ -23,14 +19,9 @@ public class CommentResponseDto {
     public CommentResponseDto(Comment comment) {
         this.commentId = comment.getId();
         this.comment = comment.getComment();
-        this.createdAt = formatter(comment.getCreatedAt());
+        this.createdAt = TimeConversion.timeConversion(comment.getCreatedAt());
         this.userId = comment.getUser().getId();
         this.nickname = comment.getUser().getNickname();
         this.profileImg = comment.getUser().getProfileImg();
     }
-
-    public String formatter(LocalDateTime localDateTime) {
-        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm").format(localDateTime);
-    }
-
 }
--- src/main/java/com/hanghae99/finalproject/timeConversion/TimeConversion.java
+++ src/main/java/com/hanghae99/finalproject/timeConversion/TimeConversion.java
@@ -2,15 +2,16 @@ package com.hanghae99.finalproject.timeConversion;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class TimeConversion {
-    public static String timeConversion(LocalDateTime modifiedAt) {
+    public static String timeConversion(LocalDateTime createdAt) {
         LocalDateTime currentTime = LocalDateTime.now();
-        Long timeDiff = Duration.between(modifiedAt, currentTime).getSeconds();
+        long timeDiff = Duration.between(createdAt, currentTime).getSeconds();
         String resultConversion = "";
 
         if ((timeDiff / 60 / 60 / 24) > 0) { // 일
-            resultConversion = timeDiff / 60 / 60 / 24 + "일 전";
+            resultConversion = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(createdAt);
         } else if ((timeDiff / 60 / 60) > 0) { // 시간
             resultConversion = timeDiff / 60 / 60 + "시간 전";
         } else if ((timeDiff / 60) > 0) { // 분
@@ -18,7 +19,6 @@ public class TimeConversion {
         } else {
             resultConversion = timeDiff + "초 전";
         }
-
         return resultConversion;
     }
 }
