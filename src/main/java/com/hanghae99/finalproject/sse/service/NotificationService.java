69e63dca8ef37c2c5af04e14e7c147ad7ee458a5
kwongyumin <rbals7398@naver.com>
2022-05-20 16:09:10 +0900

    SSE -3차 테스트

--- src/main/java/com/hanghae99/finalproject/sse/service/NotificationService.java
+++ src/main/java/com/hanghae99/finalproject/sse/service/NotificationService.java
@@ -1,7 +1,11 @@
 package com.hanghae99.finalproject.sse.service;
 
+import com.hanghae99.finalproject.sse.dto.NotificationResponseDto;
+import com.hanghae99.finalproject.sse.model.Notification;
 import com.hanghae99.finalproject.sse.repository.EmitterRepository;
 import com.hanghae99.finalproject.sse.repository.EmitterRepositoryImpl;
+import com.hanghae99.finalproject.sse.repository.NotificationRepository;
+import com.hanghae99.finalproject.user.model.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -14,12 +18,16 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class NotificationService {
     private final EmitterRepository emitterRepository = new EmitterRepositoryImpl();
+    private final NotificationRepository notificationRepository;
+
 
     public SseEmitter subscribe(Long userId, String lastEventId) {
         String emitterId = makeTimeIncludeId(userId);
         Long timeout = 60L * 1000L * 60L;
+        // userId를 기반으로 emitter를 생성
         SseEmitter emitter = emitterRepository.save(emitterId, new SseEmitter(timeout));
 
+        //시간이 만료된 후 레포에서 삭제
         emitter.onCompletion(() -> emitterRepository.deleteById(emitterId));
         emitter.onTimeout(() -> emitterRepository.deleteById(emitterId));
 
@@ -35,10 +43,13 @@ public class NotificationService {
         return emitter;
     }
 
-    private String makeTimeIncludeId(Long memberId) {
-        return memberId + "_" + System.currentTimeMillis();
+
+    // SseEmitter를 구분 -> 구분자로 시간을 사용함 ,
+    private String makeTimeIncludeId(Long userId) {
+        return userId + "_" + System.currentTimeMillis();
     }
 
+    // 유효시간이 다 지난다면 503 에러가 발생하기 때문에 더미데이터를 발행
     private void sendNotification(SseEmitter emitter, String eventId, String emitterId, Object data) {
         try {
             emitter.send(SseEmitter.event()
@@ -48,15 +59,41 @@ public class NotificationService {
             emitterRepository.deleteById(emitterId);
         }
     }
-
+   // Last - event - id 가 존재한다는 것은 받지 못한 데이터가 있다는 것이다.
     private boolean hasLostData(String lastEventId) {
         return !lastEventId.isEmpty();
     }
 
+    // 받지못한 데이터가 있다면 last - event - id를 기준으로 그 뒤의 데이터를 추출해 알림을 보내주면 된다.
     private void sendLostData(String lastEventId, Long userId, String emitterId, SseEmitter emitter) {
         Map<String, Object> eventCaches = emitterRepository.findAllEventCacheStartWithByUserId(String.valueOf(userId));
         eventCaches.entrySet().stream()
                 .filter(entry -> lastEventId.compareTo(entry.getKey()) < 0)
                 .forEach(entry -> sendNotification(emitter, entry.getKey(), emitterId, entry.getValue()));
     }
+
+    public void send(User receiver, Notification.NotificationType notificationType, String content, String url) {
+        Notification notification = notificationRepository.save(createNotification(receiver, notificationType, content, url));
+
+        String receiverId = String.valueOf(receiver.getId());
+        String eventId = receiverId + "_" + System.currentTimeMillis();
+        Map<String, SseEmitter> emitters = emitterRepository.findAllEmitterStartWithByUserId(receiverId);
+        emitters.forEach(
+                (key, emitter) -> {
+                    emitterRepository.saveEventCache(key, notification);
+                    sendNotification(emitter, eventId, key, NotificationResponseDto.create(notification));
+                }
+        );
+
+    }
+    private Notification createNotification(User receiver, Notification.NotificationType notificationType, String content, String url) {
+        return Notification.builder()
+                .receiver(receiver)
+                .notificationType(notificationType)
+                .content(content)
+                .url(url)
+                .isRead(false)
+                .build();
+    }
+
 }
