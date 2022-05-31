ea53635a72d1e9cec5bf9a8810c8c92d134d1979
kwongyumin <rbals7398@naver.com>
2022-05-31 14:17:47 +0900

    - myPage Img 서비스 수정 - 글자 제한 일시적 해제

--- src/main/java/com/hanghae99/finalproject/img/FileUploadService.java
+++ src/main/java/com/hanghae99/finalproject/img/FileUploadService.java
@@ -16,6 +16,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -38,38 +39,41 @@ public class FileUploadService {
         // 원본 이미지 업로드
         s3UploadService.uploadFile(inputStream,objectMetadata,filename);
 
-        // 리사이징 이미지 파일 이름
-        String resizedFilename = filename.replace(dirName, dirName + "-resized");
+        if(Objects.equals(dirName,"post"))
+        {
+            // 리사이징 이미지 파일 이름
+            String resizedFilename = filename.replace(dirName, dirName + "-resized");
 
-        // 사진 크기 줄이기
-        BufferedImage resizedImage = Thumbnails.of(file.getInputStream())
-                .outputQuality(1.0f)
-                .scale(0.75)
-                .asBufferedImage();
+            // 사진 크기 줄이기
+            BufferedImage resizedImage = Thumbnails.of(file.getInputStream())
+                    .outputQuality(1.0f)
+                    .scale(0.75)
+                    .asBufferedImage();
 
-        // 사진 확장자 확인
-        String extension = checkImageType(file);
+            // 사진 확장자 확인
+            String extension = checkImageType(file);
 
-        // 이미지 쓰기
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-        ImageIO.write(resizedImage, extension, os);
+            // 이미지 쓰기
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            ImageIO.write(resizedImage, extension, os);
 
-        // 에러날 경우 확장자 "png"로 처리
-        ByteArrayOutputStream uploadOs = new ByteArrayOutputStream();
-        if (os.size() == 0) {
-            ImageIO.write(resizedImage, "png", uploadOs);
-        } else {
-            uploadOs = os;
+            // 에러날 경우 확장자 "png"로 처리
+            ByteArrayOutputStream uploadOs = new ByteArrayOutputStream();
+            if (os.size() == 0) {
+                ImageIO.write(resizedImage, "png", uploadOs);
+            } else {
+                uploadOs = os;
+            }
+
+            InputStream is = new ByteArrayInputStream(uploadOs.toByteArray());
+            ObjectMetadata ob = new ObjectMetadata();
+            ob.setContentType(extension);
+            ob.setContentLength(uploadOs.size());
+
+            // 리사이징 이미지 업로드
+            s3UploadService.uploadFile(is,ob,resizedFilename);
+
         }
-
-        InputStream is = new ByteArrayInputStream(uploadOs.toByteArray());
-        ObjectMetadata ob = new ObjectMetadata();
-        ob.setContentType(extension);
-        ob.setContentLength(uploadOs.size());
-
-        // 리사이징 이미지 업로드
-        s3UploadService.uploadFile(is,ob,resizedFilename);
-
         // 원본 이미지 정보만 db에 저장
         return ImgDto.builder()
                 .imgName(filename)
