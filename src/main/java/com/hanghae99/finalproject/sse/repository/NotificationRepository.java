84b0ac2d580840fad03b9791e54fc48296fed55d
kwongyumin <rbals7398@naver.com>
2022-06-28 23:01:02 +0900

    검색 필터링_QueryDsl import 오류해결

--- src/main/java/com/hanghae99/finalproject/sse/repository/NotificationRepository.java
+++ src/main/java/com/hanghae99/finalproject/sse/repository/NotificationRepository.java
@@ -10,14 +10,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
-    @Query("select n from Notification n " +
-            "where n.receiver.id = :userId " +
-            "order by n.id desc")
+    @Query("select n from Notification n where n.receiver.id = :userId order by n.id desc")
     List<Notification> findAllByUserId(@Param("userId") Long userId);
 
-    @Query("select count(n) from Notification n " +
-            "where n.receiver.id = :userId and " +
-            "n.isRead = false")
+    @Query("select count(n) from Notification n where n.receiver.id = :userId and n.isRead = false")
     Long countUnReadNotifications(@Param("userId") Long userId);
 
     Optional<Notification> findById(Long NotificationsId);
